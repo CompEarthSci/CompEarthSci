e91e8e9974029dfa694bd8a9bc2b7b664d86a1df
Andrew Walker <andrew.walker@earth.ox.ac.uk>
2022-10-30 16:26:56 +0000

    Sketch of an example of python profiling
    Nothing very fancy. Want to see if I can get the
    line profiler working on my intel machine. Conda
    version doesn't work on M1.

---
+++ profile_example/python_pi.py
@@ -0,0 +1,25 @@
+import random
+
+
+def pi_python(num_samps, prn_seed=101):
+    """
+    Monte Carlo method of calculating pi
+  
+    num_samps is the number of samples in the unit square
+    to probe.
+  
+    """
+    random.seed(prn_seed)
+    pi = 0
+    n = 0
+    for n in range(num_samps):
+        x = random.random() 
+        y = random.random()
+        r = x**2 + y**2
+        if r <= 1.0:
+            pi += 1
+    # The area calculation ... broken up so we see things in the profile
+    pi = 4.0 * float(pi)
+    pi = pi / float(n)
+    return pi
+
