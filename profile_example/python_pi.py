2986f38306739e3c645b6385d3f1878d797cafdb
Andrew Walker <a.walker@leeds.ac.uk>
2022-11-05 16:28:20 +0000

    Changes after discussion
    We have a speedup of x10!

--- profile_example/python_pi.py
+++ profile_example/python_pi.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 
 def pi_python(num_samps, prn_seed=101):
@@ -23,3 +24,23 @@ def pi_python(num_samps, prn_seed=101):
     pi = pi / float(n)
     return pi
 
+def pi_numpy(num_samps, prn_seed=101):
+    """
+    Monte Carlo method of calculating pi
+  
+    num_samps is the number of samples in the unit square
+    to probe.
+  
+    """
+    np.random.seed(prn_seed)
+    pi = 0
+    n = 0
+    x = np.random.rand(num_samps)
+    y = np.random.rand(num_samps)
+    r = x**2 + y**2
+    boolarr = r<=1.0
+    pi = len(r[boolarr])
+    # The area calculation ... broken up so we see things in the profile
+    pi = 4.0 * float(pi)
+    pi = pi / float(num_samps)
+    return pi
